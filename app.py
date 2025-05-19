d0697c6fd0e2085e84cb4b6c8f937f1dc4faa75c
kxlu <kxlu@DESKTOP-NAOJD10>
2025-05-19 16:00:38 -0500

    html templates

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+@app.route("/pokemon")
+def pokemon():
+    return render_template('pokemon.html')
+
+@app.route("/newtrainer")
+def newtrainer():
+    return render_template('newtrainer.html')
+
+@app.route("/trainers")
+def trainers():
+    return render_template('trainers.html')
+
+@app.route("/teams")
+def teams():
+    return render_template('teams.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

dcfa7740790a577d0ccf537ce20dc064a4c0cde6
kxlu <kxlu@DESKTOP-NAOJD10>
2025-05-23 18:07:27 -0500

    Forms

--- app.py
+++ app.py
@@ -4,33 +4,32 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
-
-class SimpleForm(Form):
-    user = SelectField('User', choices=[(1, 'Admin'), (2, 'Regular')])
-    submit = SubmitField('Go')
-
-
-class MyForm(Form):
-    name = StringField('name', validators=[DataRequired()])
-
 class Base(DeclarativeBase):
   pass
 
-app = Flask(__name__)
 
 #ModuleNotFoundError: No module named 'MySQLdb'
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:Thisistp.4j!@localhost/Pokemon"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+class SimpleForm(Form):
+    user = SelectField('User', choices=[('Blank', ''), ('Admin', 'Admin'), ('Regular', 'Regular')])
+    submit = SubmitField('Go')
+
+app = Flask(__name__)
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/pokemon")
+@app.route("/pokemon", methods=['GET', 'POST'])
 def pokemon():
     form = SimpleForm(request.form)
-    return render_template('pokemon.html', form=form)
+    value = 'nothing'
+    if request.method == 'POST':
+        value = form.user.data
+    return render_template('pokemon.html', form=form, value=value)
 
 @app.route("/newtrainer")
 def newtrainer():
---
+++ load_data.py
@@ -0,0 +1,13 @@
+# Reads data from csv to initialize app dropdown options
+
+def get_moves():
+    """ 
+    Returns complete list of Pokemon moves
+    """
+    pass
+
+def get_types():
+    """ 
+    Returns complete list of Pokemon moves
+    """
+    pass
